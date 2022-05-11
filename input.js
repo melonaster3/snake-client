10f0df576daf8e195782d3f9a34b12ae3acf13cd
Sang Lee <melonaster@gmail.com>
2022-05-11 22:28:13 +0000

    added input

--- input.js
+++ input.js
@@ -18,17 +18,16 @@ const handleUserInput = function (data) {
     console.log("GOOD BYE");
     process.exit();
   }
-  if (data === 'w') {
-    connection.write("Move: up");
-  } 
-   if (data === 'a') {
-    connection.write("Move: left");  
-  } 
-   if (data === 's') {
-    connection.write("Move: down");  
-  } 
-   if (data === 'd')
-    connection.write("Move: right");
+  if (data === 'w') connection.write("Move: up");
+  if (data === 'a') connection.write("Move: left");  
+  if (data === 's') connection.write("Move: down");  
+  if (data === 'd') connection.write("Move: right");
+
+  if (data === 'z') connection.write("Say: Yalls bad");
+  if (data === 'x') connection.write("Say: :)");
+  if (data === 'c') connection.write("Say: LMAO");
+  if (data === 'v') connection.write("Say: 뭐하는짓이야!");
+
   }
 
 
