a7e3bd6e49ae02fe25ec72df6b58c4abe4241d7c
Sang Lee <melonaster@gmail.com>
2022-05-11 20:21:44 +0000

    Added play and client

--- play.js
+++ play.js
@@ -1,23 +1,8 @@
 const net = require("net");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '165.227.47.243', // IP address here,
-    port: 50541
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-conn.on('data',(data) => {
-  console.log("Incoming data says :", data);
-}) 
-
-
-  return conn;
-};
+const {connect} = require("./client.js");
 
 console.log("Connecting ...");
-connect();
 
+
+connect();
