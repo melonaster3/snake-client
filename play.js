19c97d2558faa2f4041367cd8e575271cbe8b143
Sang Lee <melonaster@gmail.com>
2022-05-11 20:51:05 +0000

    Modified play.js

--- play.js
+++ play.js
@@ -4,5 +4,24 @@ const {connect} = require("./client.js");
 
 console.log("Connecting ...");
 
+const handleUserInput = function (data) {
+  if (data === '\u0003') {
+    console.log("GOOD BYE");
+    process.exit();
+  }  
+}
+
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+
 
 connect();
+setupInput();
