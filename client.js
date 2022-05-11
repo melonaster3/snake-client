1f32b773a4f2d42345e21dd4c41d66468faec760
Sang Lee <melonaster@gmail.com>
2022-05-11 20:30:53 +0000

    added client.js

--- client.js
+++ client.js
@@ -10,6 +10,14 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
+conn.on('connect', () => {
+  console.log("Succesffully connected to game server");
+})
+
+conn.on('connect', () => {
+  conn.write("Hello from Name: SYL")
+})
+
 conn.on('data',(data) => {
   console.log("Incoming data says :", data);
 }) 
