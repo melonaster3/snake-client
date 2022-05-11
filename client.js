dcfd3d5bccb76e90dea2a4c375976df8edb2ad9c
Sang Lee <melonaster@gmail.com>
2022-05-11 22:25:11 +0000

    Added all

--- client.js
+++ client.js
@@ -1,14 +1,13 @@
 const net = require("net");
 
-// establishes a connection with the game server
 const connect = function () {
+
   const conn = net.createConnection({
-    host: '165.227.47.243', // IP address here,
+    host: '165.227.47.243', 
     port: 50541
   });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
+conn.setEncoding("utf8");
 
 conn.on('connect', () => {
   console.log("Succesffully connected to game server");
@@ -17,17 +16,9 @@ conn.on('connect', () => {
 conn.on('connect', () => {
   conn.write("Hello from Name: SYL")
 })
-
-/*setInterval(() => {
-  conn.write("Move: up");
-},1000);
-*/
-
-conn.on('data',(data) => {
-  console.log("Incoming data says :", data);
-}) 
   return conn;
 };
+
 
 
 module.exports = {connect};
--- input.js
+++ input.js
@@ -1,12 +1,10 @@
-const handleUserInput = function (data) {
-  if (data === '\u0003') {
-    console.log("GOOD BYE");
-    process.exit();
-  }  
-}
+const net = require("net");
 
 
-const setupInput = function () {
+let connection; 
+
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -14,6 +12,24 @@ const setupInput = function () {
   stdin.on("data", handleUserInput);
   return stdin;
 };
+
+const handleUserInput = function (data) {
+  if (data === '\u0003') {
+    console.log("GOOD BYE");
+    process.exit();
+  }
+  if (data === 'w') {
+    connection.write("Move: up");
+  } 
+   if (data === 'a') {
+    connection.write("Move: left");  
+  } 
+   if (data === 's') {
+    connection.write("Move: down");  
+  } 
+   if (data === 'd')
+    connection.write("Move: right");
+  }
 
 
 module.exports = { setupInput };
